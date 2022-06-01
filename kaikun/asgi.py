8f8fe85ec1ec128b11da7b493401d3f82d0a8f89
shal5ho4 <rcxsoor@gmail.com>
2022-06-02 00:33:16 +0900

    initial commit

---
+++ kaikun/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for kaikun project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kaikun.settings')
+
+application = get_asgi_application()
