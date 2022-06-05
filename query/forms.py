d31d37cc17778451bf93504ecb383ab892a657b8
OnePercent <onepercent@gmail.com>
2022-06-05 22:22:36 +0900

    フォーム完成

--- query/forms.py
+++ query/forms.py
@@ -5,4 +5,21 @@ class QueryForm(forms.ModelForm):
   class Meta:
     model = Query
     fields = ['name', 'email', 'phone', 'text']
-    
+    widgets = {
+      'name': forms.TextInput(
+        attrs={'class': 'tm-input', 'placeholder': 'Name'}
+      ),
+      'email': forms.TextInput(
+        attrs={'class': 'tm-input', 'placeholder': 'Email'}
+      ),
+      'phone': forms.TextInput(
+        attrs={'class': 'tm-input', 'placeholder': 'Tel'}
+      ),
+      'text': forms.Textarea(
+        attrs={
+          'class': 'tm-input', 
+          'placeholder': 'Text',
+          'rows': 8
+        }
+      )
+    }
--- query/views.py
+++ query/views.py
@@ -5,20 +5,32 @@ def query_form(request):
   if request.method == 'POST':
     form = QueryForm(request.POST)
 
-    if request.POST.get('next', '') == 'confirm':
-      form.save(commit=False) #いる？
-      return render(
-        request, 'query/form_create.html', {'form': form}
-      )
     if request.POST.get('next', '') == 'back':
-      form = QueryForm(request.session.get('form_data'))
-      return render(
-        request, 'query/form.html', {'form': form}
-      )
-    if request.POST.get('next', '') == 'create':
+      if 'input_data' in request.session:
+        input_data = request.session['input_data']
+        form = QueryForm(input_data)
+      return render(request, 'query/form.html', {'form': form})
+
+    elif request.POST.get('next', '') == 'confirm':
+      form = QueryForm(request.POST)
+      if form.is_valid():
+        input_data = {
+          'name': form.cleaned_data['name'],
+          'email': form.cleaned_data['email'],
+          'phone': form.cleaned_data['phone'],
+          'text': form.cleaned_data['text']
+        }
+        request.session['input_data'] = input_data
+        return render(request, 'query/form_create.html', {'form': form})
+
+    elif request.POST.get('next', '') == 'create':
+      form = QueryForm(request.POST)
       form.save(commit=True)
+      request.session.pop('input_data')
+      #メール処理
+      #通知処理
       return render(request, 'query/confirmation.html')
-  
+
   else:
     form = QueryForm()
-  return render(request, 'query/form.html')
+    return render(request, 'query/form.html', {'form': form})
