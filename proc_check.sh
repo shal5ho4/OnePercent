6f8edf36c447916452ecff6fcd9997f1efb8629c
shal5ho4 <90442134+shal5ho4@users.noreply.github.com>
2022-08-25 10:14:19 +0900

    Update proc_check.sh

--- proc_check.sh
+++ proc_check.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
   
 proc1="/usr/sbin/rabbitmq-server"
-proc2="/home/onepercentadmin/django_projects/OnePercent/env/bin/celery"
-proc3="/usr/sbin/nginx"
+proc2="/usr/sbin/nginx"
+proc3="/home/onepercentadmin/django_projects/OnePercent/env/bin/celery"
+proc4="/home/onepercentadmin/django_projects/OnePercent/env/bin/gunicorn"
 
-count1=$(ps ax -o command | grep "$proc1" | grep -v "^grep" | wc -l)
-count2=$(ps ax -o command | grep "$proc2" | grep -v "^grep" | wc -l)
-count3=$(ps ax -o command | grep "$proc3" | grep -v "^grep" | wc -l)
 
-for ((i=1; i<=3; i++)); do
+for ((i=1; i<=4; i++)); do
     proc="proc${i}"
-    count="count${i}"
+    count=$(ps ax -o command | grep "${!proc}" | grep -v "^grep" | wc -l)
 
-    if [ ${!count} -eq 0 ]; then
-        echo "[ERROR] process $proc is down" >&2
-        /home/onepercentadmin/alert.sh "[ERROR] process $proc has been down"
+    if [ $count -eq 0 ]; then
+        echo "[ERROR] process ${!proc} is down" >&2
+        /home/onepercentadmin/alert.sh "[ERROR] process ${!proc} has been down"
     fi
 done
